9cb9ef5d41d4a0d61a90e987ffcee348e0b8c081
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2024-05-06 17:35:54 +0300

    sql server responce

---
+++ CategoriesAndProducts/Create.sql
@@ -0,0 +1,19 @@
+﻿create table Products (
+	Id int primary key identity,
+	Name nvarchar(50) unique,
+);
+
+go
+
+create table Categories (
+	Id int primary key identity,
+	Name nvarchar(50) unique not null
+);
+
+go
+
+create table ProductsCategories (
+	ProductId int foreign key references Products(Id),
+	CategoryId int foreign key references Categories(Id),
+	primary key(ProductId, CategoryId)
+);
---
+++ CategoriesAndProducts/GetProducts.sql
@@ -0,0 +1,10 @@
+﻿
+select 
+	pairs.ProductName,
+	(select [Name] from Categories where Id = pairs.CategoryId) as CategoryName
+
+from 
+	(select 
+		[Name] as ProductName,
+		(select [CategoryId] from ProductsCategories where ProductId = Products.Id) as CategoryId
+	from Products) pairs
